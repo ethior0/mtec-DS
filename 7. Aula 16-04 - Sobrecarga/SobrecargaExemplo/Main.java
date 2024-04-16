d1f4ea34f4067d4f72adf4a8d6811404f529e940
thiago.dsm <133548984+ethior0@users.noreply.github.com>
2024-04-16 08:32:31 -0300

    Update Main.java

--- 7. Aula 16-04 - Sobrecarga/SobrecargaExemplo/Main.java
+++ 7. Aula 16-04 - Sobrecarga/SobrecargaExemplo/Main.java
@@ -1,19 +1,9 @@
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] arg) {
 		
-		Calculadora calc = new Calculadora();
-		calc.setNome("Calc TOP");
-		
-		// Soma
-		System.out.println(calc.soma( 12, 42 ));
-		System.out.println(calc.soma( 24.3, 1.4 ));
-		System.out.println(calc.soma( new int[] {10, 20, 5} ));
-		
-		// Subtração
-		System.out.println(calc.subtracao( 1, 1 ));
-		System.out.println(calc.subtracao( 10.9, 0.2 ));
-		System.out.println(calc.subtracao( new int[] {50, 1, 2} ));
+		Professor teste1 = new Professor();
+		Professor teste2 = new Professor(20, "Doni");
 	}
 }
